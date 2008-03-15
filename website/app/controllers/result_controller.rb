354f4e431db8a1936d3dca847091760cd508b743
gigix1980 <gigix1980@ae7a3ec7-be47-0410-8f89-b7ae3a7bdf16>
2008-03-15 16:41:25 +0000

    writing rails plugin

--- website/app/controllers/result_controller.rb
+++ website/app/controllers/result_controller.rb
@@ -3,12 +3,15 @@ class ResultController < ApplicationController
   REPORT_DIR = File.join RAILS_ROOT, 'report'
   
   def report
+    status = params[:status]
     data = params[:data]
     FileUtils.mkdir_p REPORT_DIR
+    
     report_file = File.join REPORT_DIR, Time.now.to_i.to_s
-    File.open(report_file, "w") do |f|
-      f.write data
-    end
+    File.open(report_file, 'w'){|f| f.write data }
+    status_file = File.join REPORT_DIR, 'status'
+    File.open(status_file, 'w'){|f| f.write status }
+        
     render :text => data
   end
 end
